55fe4f8642831b6919dd56dd03fc371745557ab1
Claudia Mosca <mosca.claud@gmail.com>
2019-05-16 21:31:26 +0100

    Update Yelp.js

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,5 +1,5 @@
-const clientId = 'foYKL-RKbEtxt2rVYFWjGg';
-const secret = 'dNdOBTYmwJ2RgfLr1veCIXGYgFUz38LkMEQlgNv_HjDmF2KK3TAd9A__54dmWkKTWYEJ82CWpEuIqGwBLq7GlRaWqMw-bJKh8FxizQyv9LLK4gDmFJRzvdb7fVncXHYx';
+const clientId = '';
+const secret = '';
 let accessToken;
 
 const Yelp = {
@@ -9,21 +9,21 @@ const Yelp = {
           return new Promise(resolve => resolve(accessToken));
         }
         //Make a request to the Yelp API token endpoint and pass clientId and secret to get the access token.
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, { 
-            method: 'POST'
-           }).then(response => {
-            return response.json();
-           }).then(jsonResponse => {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, 
+           {  method: 'POST' })
+          .then(response => response.json())
+          .then(jsonResponse => {
             accessToken = jsonResponse.access_token;
            });
         },
     search: function(term, location, sortBy) {
-        return Yelp.getAccessToken().then(() => {
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
-            headers: { Authorization: `Bearer ${accessToken}` }
-        }).then(response => { 
-            return response.json();
-        }).then(jsonResponse => {
+        return Yelp.getAccessToken()
+          .then(() => {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, 
+                     { headers: { Authorization: `Bearer ${accessToken}` }
+        })
+         .then(response => response.json())
+         .then(jsonResponse => {
               if (jsonResponse.businesses) {
                   return jsonResponse.businesses.map(business => {
                       console.log(business);
@@ -45,7 +45,24 @@ const Yelp = {
        });
     
     }
-};
+},
+
+  /**
+   * Add functionality to provide users with the ability to view reviews.
+   * @param {*} id 
+   */
+  getReviews(id) {
+    return Yelp.getAccessToken()
+      .then(() => {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/${id}/reviews`, { headers: { Authorization: `Bearer ${accessToken}`}})
+      })
+      .then(response => response.json())
+      .then(jsonResponse => {
+        return jsonResponse.reviews;
+      });
+
+  }
+}
 
 export default Yelp;
 
